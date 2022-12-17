3013ff1bbe8ddd6cd3e3594f14aeeee269db20d4
Carl Thomé <carlthome@gmail.com>
2022-12-17 20:11:21 +0100

    Set device to cuda if available

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import torch
 from diffusers import StableDiffusionPipeline
 
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# TODO mps if macOS M1
+
 pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
-pipe = pipe.to("mps")
+pipe = pipe.to(device)
 pipe.enable_attention_slicing()
 
 prompt = input("Text prompt: ")
