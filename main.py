1195b57648ad7878c43462bdd1dfa3ff0d104b7e
Carl Thomé <carlthome@gmail.com>
2024-01-03 14:50:09 +0100

    Wrap script in main function

--- main.py
+++ main.py
@@ -3,41 +3,51 @@ import argparse
 import torch
 from diffusers import StableDiffusionPipeline, StableDiffusionXLPipeline
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--prompt", type=str)
-parser.add_argument("--xl", action="store_true")
-parser.add_argument("--turbo", action="store_true")
-args = parser.parse_args()
 
-if args.prompt:
-    prompt = args.prompt
-else:
-    prompt = input("Text prompt: ")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prompt", type=str)
+    parser.add_argument("--xl", action="store_true")
+    parser.add_argument("--turbo", action="store_true")
+    args = parser.parse_args()
 
-if torch.backends.mps.is_available():
-    device = torch.device("mps")
-elif torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-print(f"{device=}")
+    if args.prompt:
+        prompt = args.prompt
+    else:
+        prompt = input("Text prompt: ")
 
-if args.xl:
-    pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0")
-elif args.turbo:
-    pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
-else:
-    pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    print(f"{device=}")
 
-pipe = pipe.to(device)
-pipe.enable_attention_slicing()
+    if args.xl:
+        pipe = StableDiffusionXLPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0"
+        )
+    elif args.turbo:
+        pipe = StableDiffusionXLPipeline.from_pretrained(
+            "stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16"
+        )
+    else:
+        pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
 
-settings = {}
+    pipe = pipe.to(device)
+    pipe.enable_attention_slicing()
 
-if args.turbo:
-    settings["num_inference_steps"] = 1
-    settings["guidance_scale"] = 0.0
+    settings = {}
 
-image = pipe(prompt, **settings).images[0]
+    if args.turbo:
+        settings["num_inference_steps"] = 1
+        settings["guidance_scale"] = 0.0
 
-image.save(f"{prompt.lower().replace(' ', '_')}.png")
+    image = pipe(prompt, **settings).images[0]
+
+    image.save(f"{prompt.lower().replace(' ', '_')}.png")
+
+
+if __name__ == "__main__":
+    main()
